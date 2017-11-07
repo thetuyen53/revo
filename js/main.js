2001163bd8453c5a9ca1142930e7b921586bf45d
Tuyen Nguyen <tuyen.nguyen@bstarsolutions.com>
2017-11-07 17:53:32 +0700

    Layout,

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.product-slick').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  prevArrow: $(".post-nav-l"),
+  nextArrow: $(".post-nav-r")
+});
